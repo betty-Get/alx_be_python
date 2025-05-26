409d1447613bfa49e683c5906cc017da71dd773a
bettyGpt <betelhem.getachew@girlpowertalk.com>
2025-05-26 16:27:19 +0300

    simple python calculation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number1 - number2
+multiply = number1 * number2
+
+# print(sum)
+print("Addition of 10 and 5 is", sum)
+print("Subtraction of 10 and 5 is 5", difference)
+print("Multiplication of 10 and 5 is", multiply)
