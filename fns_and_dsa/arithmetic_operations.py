301d5151a2a5fc406cd916b38af66233a7100256
bettyGpt <betelhem.getachew@girlpowertalk.com>
2025-06-11 16:26:33 +0300

    funtion added

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,16 @@
 def perform_operation(num1, num2, operation):
     if operation == add:
         result = num1 + num2
-        return result
     elif operation == subtract:
         result = num1 - num2
-        return result
+       # return result
     elif operation == multiply:
         result = num1 * num2
-        return result
+       # return result
     else:
-        result = num1 / num2
-        return result
+        if num2 == 0:
+            print("the second number can not be a zero")
+         #   return
+        else:
+            result = num1 / num2
+        #    return result
