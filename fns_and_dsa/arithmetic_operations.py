7024e816d6b044d971020d6a0650754c0e1e236b
bettyGpt <betelhem.getachew@girlpowertalk.com>
2025-06-11 16:20:29 +0300

    funtion added

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    if operation == add:
+        result = num1 + num2
+        return result
+    elif operation == subtract:
+        result = num1 - num2
+        return result
+    elif operation == multiply:
+        result = num1 * num2
+        return result
+    else:
+        result = num1 / num2
+        return result
