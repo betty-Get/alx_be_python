79a6767af1551ff7ae6c27ed79dad27d8705fd07
bettyGpt <betelhem.getachew@girlpowertalk.com>
2025-06-11 22:25:52 +0300

    time added

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def display_current_datetime():
     current_date = datetime.now()
     # current_date.strftime("%X")
-    print(current_date)
+    print(f"Current date and time: {current_date}")
 
 
 display_current_datetime()
