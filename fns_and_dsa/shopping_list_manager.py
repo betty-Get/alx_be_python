8389617e0493f848065a5ba6c7e608529d1d48b2
bettyGpt <betelhem.getachew@girlpowertalk.com>
2025-06-11 17:22:08 +0300

    input modified

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,11 +10,11 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
+        choice = input("Enter your choice: ")
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("please input an item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == '2':
             # Prompt for and remove an item
