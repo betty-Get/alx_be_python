c39cce98d821ece9f92f22b6571b2023adfbcb4d
bettyGpt <betelhem.getachew@girlpowertalk.com>
2025-06-12 16:48:26 +0300

    temp conversion added

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,23 +1,3 @@
-# from datetime import datetime, timedelta
-
-
-# def display_current_datetime():
-#     global current_date
-#     current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     # current_date.strftime("%X")
-#     print(f"Current date and time: {current_date}")
-
-
-# def calculate_future_date():
-#     date_to_add = int(input(
-#         "Enter the number of days to add to the current date: "))
-#     future_date = int(current_date) + timedelta(days=date_to_add)
-#     print(f"Future date: {future_date}")
-
-
-# display_current_datetime()
-# calculate_future_date()
-
 from datetime import datetime, timedelta
 
 
---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,23 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+user_input = int(input("Enter the temperature to convert: "))
+temp_input = input(
+    "Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().lower()
+
+if temp_input == f:
+    convert_to_celsius(user_input)
+elif temp_input == c:
+    convert_to_fahrenheit(user_input)
+else:
+    print("please input a proper Temprature")
+
+
+def convert_to_celsius(fahrenheit):
+    converted_temp = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f"{user_input}°F is {converted_temp}°C")
+
+
+def convert_to_fahrenheit(celsius):
+    converted_temp = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(f"{user_input}°C is {converted_temp}°F")
